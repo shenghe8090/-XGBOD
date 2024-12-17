ec23f02ef97daea89c7809eff2a5f48ac38c81de
shenghe8090 <117013007+shenghe8090@users.noreply.github.com>
2024-12-17 17:58:35 +0800

    Update FileInputOutput.py

--- XGBOD-main/utils/modules/FileInputOutput.py
+++ XGBOD-main/utils/modules/FileInputOutput.py
@@ -2,8 +2,8 @@
 #python code for geochemical anomaly extraction
 #Written By:
 #Yongliang Chen
-
 # Note: it is not authorised to be made public, so please understand that it is blank.
+
 import numpy as np
 import math
 import struct
